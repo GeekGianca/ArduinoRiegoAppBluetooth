17675b30ca57a6f22203899e24a319928ab4c146
Geek Programmer <giancarlos.cuello@gmail.com>
2018-05-04 11:55:37 -0500

    Fixed on-off port and add new port

--- RiegoAppArduino.ino
+++ RiegoAppArduino.ino
@@ -1,40 +1,44 @@
-#include <SoftwareSerial.h> 
- 
-SoftwareSerial ModBluetooth(4, 2); // RX | TX 
- 
-void setup()  
-{ 
-    pinMode(13, OUTPUT); 
-    digitalWrite(13, LOW);  
-     
-    ModBluetooth.begin(9600); 
-    Serial.begin(9600);  
-    ModBluetooth.println("MODULO CONECTADO");  
-    ModBluetooth.print("#");  
-} 
- 
-void loop()  
-{  
-    if (ModBluetooth.available())  
-    { 
-        char VarChar; 
-         
-        VarChar = ModBluetooth.read(); 
-         
-        if(VarChar == '1') 
-        { 
-        digitalWrite(13, HIGH); 
-        delay(100); 
-        ModBluetooth.print("LED ENCENDIDO"); 
-        Serial.print("LED ENCENDIDO"); 
-        ModBluetooth.print("#"); 
-        } 
-        if(VarChar == '0') 
-        { 
-        digitalWrite(13, LOW); 
-        delay(100); 
-        ModBluetooth.print("LED APAGADO#"); 
-        Serial.print("LED APAGADO#"); 
-        } 
-    } 
-} 
+#include <SoftwareSerial.h>
+
+SoftwareSerial ModBluetooth(2, 3); // RX | TX
+int relay = 12;
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  pinMode(relay, OUTPUT);
+  ModBluetooth.begin(9600);
+  Serial.begin(9600);
+  ModBluetooth.println("Modulo Conectado");
+  ModBluetooth.print("#");
+}
+
+
+void loop()
+{
+  if (ModBluetooth.available())
+  {
+    char VarChar;
+
+    VarChar = ModBluetooth.read();
+
+    if (VarChar == '1')
+    {
+      digitalWrite(13, HIGH);
+      digitalWrite(relay, HIGH);
+      delay(100);
+      ModBluetooth.print("Riego Encendido");
+      Serial.println("Riego Encendido");
+      ModBluetooth.print("#");
+    }
+    if (VarChar == '0')
+    {
+      digitalWrite(13, LOW);
+      digitalWrite(relay, LOW);
+      delay(100);
+      ModBluetooth.print("Riego Apagado#");
+      Serial.println("Riego Apagado#");
+    }
+  }
+}
